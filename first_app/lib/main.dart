7397294b4b547f2d7095c7e2bde391b45389730b
kinny <ad.sam007@gmail.com>
2019-05-27 21:35:13 -0400

    add dynamic stateful lements

--- first_app/lib/main.dart
+++ first_app/lib/main.dart
@@ -4,7 +4,16 @@ void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+
+  List<String> _products = ['Food Tester'];
 
   @override
   Widget build(BuildContext context) {
@@ -13,10 +22,25 @@ class MyApp extends StatelessWidget {
         appBar: AppBar(
           title: Text('Easylist'),
         ),
-        body: Card(child: Column(children: <Widget>[
-          Image.asset('assets/food.jpg'),
-          Text('Food Paradise')
-        ],),)
+        body: Column(
+          children: [
+            Container(
+              margin: EdgeInsets.all(10.0),
+              child: RaisedButton(
+                onPressed: () {},
+                child: Text('Add Product'),
+              ),
+            ),
+            Column(children: _products.map((element) =>  Card(
+              child: Column(
+                children: <Widget>[
+                  Image.asset('assets/food.jpg'),
+                  Text(element)
+                ],
+              ),
+            ),).toList(),
+          )
+        ],),
       ),
     );
   }
