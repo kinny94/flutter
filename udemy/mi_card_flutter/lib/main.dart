e19957467c4512e6f6b5428140609860c7b4b3a5
kinny <ad.sam007@gmail.com>
2019-10-19 09:30:27 -0400

    feat: column and rows

--- udemy/mi_card_flutter/lib/main.dart
+++ udemy/mi_card_flutter/lib/main.dart
@@ -6,15 +6,30 @@ void main() {
       home: Scaffold(
         backgroundColor: Colors.teal,
         body: SafeArea(
-          child: 
-          Container(
-            height: 100.0,
-            width: 100.0,
-            margin: EdgeInsets.symmetric(vertical: 50.0, horizontal: 10.0),
-            padding: EdgeInsets.all(20.0),
-            color: Colors.white,
-            child: Text('Hello')
-          ), 
+          child: Row(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              Container(
+                height: 100.0,
+                width: 100.0,
+                color: Colors.white,
+                child: Text('Hello')
+              ), 
+              SizedBox(height: 20, width: 20.0,),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.blue,
+                child: Text('Container 2'),
+              ),
+              Container(
+                width: 100.0,
+                height: 100.0,
+                color: Colors.red,
+                child: Text('Container 3'),
+              ),
+            ],
+          )
         ),
       ),
     ),
