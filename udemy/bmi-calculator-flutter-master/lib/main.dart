a4ed7af08ab722f8bf9fb6106755a4c3688ee901
kinny <ad.sam007@gmail.com>
2019-11-05 21:48:02 -0500

    chan ge the slider

--- udemy/bmi-calculator-flutter-master/lib/main.dart
+++ udemy/bmi-calculator-flutter-master/lib/main.dart
@@ -8,6 +8,13 @@ class BMICalculator extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       theme:  ThemeData.dark().copyWith(
+        sliderTheme: SliderTheme.of(context).copyWith(
+          activeTrackColor: Colors.white,
+          thumbColor: Color(0xFFEB1555),
+          overlayColor:  Color(0x15EB1555),
+          thumbShape: RoundSliderThumbShape(enabledThumbRadius: 15.0),
+          overlayShape: RoundSliderOverlayShape(overlayRadius: 30.0),
+        ),
         primaryColor: Color(0xFF0A0E21),
         scaffoldBackgroundColor: Color(0xFF0A0E21),
       ),
